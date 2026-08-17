0089fc4c63089c339c0fd143d67eeaee7cf3cab3
Berkay Öner <berkayonerr2002@gmail.com>
2026-08-17 17:47:05 +0300

    Minor bug fixes for security.

--- api/src/main/java/com/berkay/api/config/CorsConfig.java
+++ api/src/main/java/com/berkay/api/config/CorsConfig.java
@@ -34,6 +34,7 @@ public class CorsConfig implements WebMvcConfigurer {
 		registry.addMapping("/**")
 				.allowedOriginPatterns(corsProperties.getPublicAllowedOrigins().toArray(String[]::new))
 				.allowedMethods("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS")
-				.allowedHeaders("*");
+				.allowedHeaders("*")
+				.allowCredentials(true);
 	}
 }
